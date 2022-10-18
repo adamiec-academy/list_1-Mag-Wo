da2ce2b64f4648948cd1a952b91d781259e61fbf
Mag-Wo <115737730+Mag-Wo@users.noreply.github.com>
2022-10-18 20:40:42 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,9 @@
-def cross(n):
-    pass
+def cross(n,a,b):
+    for i in range(n):
+        print(4*" "+n*"*")
+    for i in range(b):
+        print(a * "*")
+    for i in range(n):
+        print(4*" "+n*"*")
+
+cross(4,12,4)
