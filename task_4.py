64c58553753c4959fca13db343378352680c203b
Mag-Wo <115737730+Mag-Wo@users.noreply.github.com>
2022-10-19 00:18:45 +0200

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,6 +1,12 @@
-def snowball(n, k):
-    pass
+def snowball(n,k):
 
+    for i in range(2,8,2):
+        print(k*" "+(n-i-1)//2 * " " +(i+1)*"#")
+    print(k*" "+ "#"*n)
+    for i in range(2,8,2):
+        print(k*" "+((2*(i+n)//(n//2))-6) * " "+ (n-i+2)*"#")
+
+snowball(7,10)
 
 def snowman(n):
     pass
