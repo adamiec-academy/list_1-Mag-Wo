f33816b8c6dfe5ef421ca242b38b2d0550f418bb
Mag-Wo <115737730+Mag-Wo@users.noreply.github.com>
2022-10-18 20:47:22 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,8 @@
-def chess_board(n, k):
-    pass
+def chessboard(n):
+    for i in range(n):
+        for i in range(1,2):
+            print((" "+"##")*4)
+        for i in range(3,4):
+            print(("##"+" ")*4)
+
+chessboard(8)
