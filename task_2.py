d3900f88a66bf23eed422e46ba9cd19e30283264
Mag-Wo <115737730+Mag-Wo@users.noreply.github.com>
2022-11-20 11:53:12 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -2,11 +2,7 @@ def chessboard(n, k):
 
     for i in range(n):
         for _ in range(k):
-            print(((k * ' ') + (k * '#')) * n)
+            print((k * " " + k * "#") * n)
         for _ in range(k):
-            print(((k * '#') + (k * ' ')) * n)
+            print((k * "#" + k * " ") * n)
 
-
-n = 4
-k = 2
-chessboard(n, k)
