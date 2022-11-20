0fdfc1cab0ba522e70747a8bc7c3e44d2b71e732
Mag-Wo <115737730+Mag-Wo@users.noreply.github.com>
2022-11-20 11:18:16 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,9 +1,9 @@
 def cross(n):
     for i in range(n):
-        print(" " * n + "*" * n + " " * n)
+        print(' ' * n + '*' * n + ' ' * n)
     for i in range(n):
-        print("*" * 3 * n)
+        print('*' * 3 * n)
     for i in range(n):
-        print(" " * n + "*" * n + " " * n)
+        print(' ' * n + '*' * n + ' ' * n)
 
-cross(5)
+cross(n)
