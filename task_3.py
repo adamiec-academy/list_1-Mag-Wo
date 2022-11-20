a8d643d6fd48410513d0859a64d8c4d53c37104a
Mag-Wo <115737730+Mag-Wo@users.noreply.github.com>
2022-11-20 11:24:17 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,14 +1,1 @@
-def envelope(n):
-    print((2*n+1)*"*")
 
-    for i in range(n-1):
-        print("*" + i * " " + "*" + (2*n-3-2*i) * " " + "*" + i * " " + "*")
-
-    print("*" + (n-1) * " " + "*" + (n-1) * " " + "*")
-
-    for i in range(n-1):
-        print("*" + (n-2-i) * " " + "*" + (2*i+1) * " " + "*" + (n-2-i) * " " + "*")
-
-    print((2*n+1)*"*")
-
-envelope(4)
